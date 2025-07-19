f66c809906164a9be399638539ce4784082cd454
raquel boaventura <raquelb_moraes@live.com>
2025-07-18 22:04:51 -0300

    emprestimos, cadastro e listagem

--- src/main/java/com/example/colina/dto/LivroDTO.java
+++ src/main/java/com/example/colina/dto/LivroDTO.java
@@ -1,18 +1,13 @@
 package com.example.colina.Dto;
 
-import com.example.colina.entity.Emprestimo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class LivroDTO {
-    private Long id_livro;
+    private Long id;
     private Long isbn;
     private String titulo;
     private String autor;
@@ -23,5 +18,4 @@ public class LivroDTO {
     private String genero;
     private int quantidade;
     private boolean status = true;
-    private List<Emprestimo> emprestimos = new ArrayList<>();
 }
